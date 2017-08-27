07bdab3426edae54192f22acc10a8a8e3e3abc0e
mrdulin <novaline@qq.com>
2017-08-27 20:37:05 +0800

    mongodb数据库添加用户权限

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const config = {
   COOKIE_SECRET: '7WN67f36',
-  DB_URI: 'mongodb://localhost:27017/typescript-mongoose-express-starter',
+  DB_URI: 'mongodb://root:365715693@localhost:27017/typescript-mongoose-express-starter',
   DEFAULT_PORT: '2222',
   DOMAIN: 'localhost',
   MAX_AGE: 60 * 60 * 1000,
