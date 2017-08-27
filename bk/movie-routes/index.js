6f54b8546e8b5532cd61f679c43fcdc0de7c03bd
mrdulin <novaline@qq.com>
2017-08-27 20:06:34 +0800

    删除无用的文件，添加项目说明

--- bk/movie-routes/index.js
+++
@@ -1,7 +0,0 @@
-var router = require('express').Router();
-require(__base + 'middlewares/parser')(router);
-
-router.use('/actors', require('./actors'));
-router.use('/movies', require('./movies'));
-
-module.exports = router;
